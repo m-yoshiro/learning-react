b7e79c002eb30dff3b43c3c5b6681704bfc48376
yoshiro4444 <massugu.massugu@gmail.com>
2016-04-24 15:44:55 +0900

    lane関連

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,66 +1,26 @@
-import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-  }
   render() {
-    const notes = this.state.notes;
-
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+        <button className="add-lane" onClick={this.addLane}>+</button>
+        <AltContainer
+          stores={[LaneStore]}
+          inject={{
+            lanes: () => LaneStore.getState().lanes || []
+          }}
+        >
+          <Lanes />
+        </AltContainer>
       </div>
     );
   }
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New tast'
-      }])
-    });
-  };
-  editNote = (id, task) => {
-
-    if (!task.trim()) {
-      return;
-    }
-
-    const notes = this.state.notes.map(note => {
-      if (note.id === id && task) {
-        note.task = task;
-      }
-
-      return node;
-    });
-
-    this.setState({notes});
-  };
+  addLane() {
+    LaneActions.create({name: 'New lane'});
+  }
 }
---
+++ app/components/Lane.jsx
@@ -0,0 +1,82 @@
+import AltContainer from 'alt-container';
+import React from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+import LaneActions from '../actions/LaneActions';
+import Editable from './Editable.jsx';
+
+export default class Lane extends React.Component {
+  render() {
+    const {lane, ...props} = this.props;
+
+    return (
+      <div {...props}>
+        <div className="lane-header" onClick={this.activateLaneEdit}>
+          <div className="lane-add-note">
+            <button onClick={this.addNote}>+</button>
+          </div>
+          <Editable className="lane-name" editing={lane.editing}
+            value={lane.name} onEdit={this.editName} />
+          <div className="lane-delete">
+            <button onClick={this.deleteLane}>x</button>
+          </div>
+        </div>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getNotesByIds(lane.notes)
+          }}
+        >
+          <Notes
+            onValueClick={this.activateNoteEdit}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    )
+  }
+  editNote(id, task) {
+    if (!task.trim()) {
+      return;
+    }
+
+    NoteActions.update({id, task});
+  }
+  addNote = (e) => {
+    e.stopPropagation();
+    const laneId = this.props.lane.id;
+    const note = NoteActions.create({task: 'New task'});
+
+    LaneActions.attachToLane({
+      noteId: note.id,
+      laneId
+    });
+  };
+  deleteNote(id, e) {
+    e.stopPropagation();
+
+    const laneId = this.props.lane.id;
+
+    LaneActions.detachFromLane({laneId, noteId});
+    NoteActions.delete(noteId);
+  };
+  editName = (name) => {
+    const laneId = this.props.lane.id;
+
+    console.log(`edit lane ${laneId} name using ${name}`);
+  };
+  deleteLane = () => {
+    const laneId = this.props.lane.id;
+
+    console.log(`delete lane ${laneId}`);
+  };
+  activateLaneEdit = () => {
+    const laneId = this.props.lane.id;
+    console.log(`activate lane ${laneId} edit`);
+  };
+  activateNoteEdit(id) {
+    console.log(`activate note ${laneId} edit`);
+  }
+}
+
